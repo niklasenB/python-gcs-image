f83e81e37e030c38813ee150133f0b9e801be840
Niklas Wohlgemuth <niklas.wohlgemuth@netnauten.de>
2022-02-11 21:21:47 +0100

    Configuration for GAE

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,11 @@
 """Add the lib directory to the path, so you can use libraries."""
+# appengine_config.py
+from google.appengine.ext import vendor
+
 
 import sys
 import os.path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+#sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+
+# Add any libraries install in the "lib" folder.
+vendor.add('lib')
